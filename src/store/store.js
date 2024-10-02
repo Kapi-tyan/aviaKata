7c7589eaa6e650ff0391a3d907dc7fec63b8bf85
Kapi-tyan <164524421+Kapi-tyan@users.noreply.github.com>
2024-10-02 21:15:12 +0500

    Update store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import { logger } from '../middleware/logger';
+//import { logger } from '../middleware/logger';
 
 import AsideBacheckboxFilters from './slices/AsideBarSlice';
 import TicketsButtonSlice from './slices/TicketsButtonSlice';
@@ -12,5 +12,5 @@ export const store = configureStore({
     ticketsButton: TicketsButtonSlice,
     tickets: GetTickets,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  //middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
